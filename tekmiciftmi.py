708bb1bb115a6a1ecacdfd9d40d1fcc8b2fcca69
niluferbozkus <72705898+niluferbozkus@users.noreply.github.com>
2020-10-21 18:26:26 +0300

    Add files via upload

---
+++ tekmiciftmi.py
@@ -0,0 +1,7 @@
+#Bir sayının tek mi çift mi olduğunu tespit eden programı yazıyoruz.
+
+sayı = int(input("Bir sayı girin: "))
+if sayı % 2 == 0:
+ print("Girdiğiniz sayı çift sayıdır.")
+else:
+ print("Girdiğiniz sayı tek sayıdır.")
