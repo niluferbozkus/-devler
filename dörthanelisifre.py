fc6465926c9e8720161e786ac972a623f75609be
niluferbozkus <72705898+niluferbozkus@users.noreply.github.com>
2020-11-05 18:28:34 +0300

    Add files via upload

---
+++ dörthanelisifre.py
@@ -0,0 +1,10 @@
+x = str(input("4 haneli bir şifre belirleyiniz: "))
+    
+while len(x)!=4:
+    print("Lütfen girdiğiniz şifrenin 4 haneli olduğundan emin olun!")
+    x = str(input("4 haneli bir şifre belirleyiniz: "))
+
+print("Şifreniz kaydedilmiştir.")
+
+    
+
