d86ae7b3a36e6f2494f6a771a76b28a240933de3
niluferbozkus <72705898+niluferbozkus@users.noreply.github.com>
2020-11-11 09:11:31 +0300

    Update dörthanelisifre.py

--- dörthanelisifre.py
+++ dörthanelisifre.py
@@ -1,12 +1,8 @@
 x = input("4 haneli bir şifre belirleyiniz: ")
-    
+
+#Girdi olarak alınan değerin 4 haneli ve sadece rakamlardan oluşan bir şifre olduğunun kontrolü   
 while len(x)!=4 or x.isdigit() != 1 :
-    print("Lütfen girdiğiniz şifrenin 4 haneli ve rakamlardan oluşaan bir şifre olduğundan emin olun!")
-    x = str(input("4 haneli bir şifre belirleyiniz: "))
+    print("Lütfen girdiğiniz şifrenin 4 haneli olduğundan ve sadece rakamlardan oluştuğundan emin olun!")
+    x = input("4 haneli bir şifre belirleyiniz: ")
 
 print("Yeni şifreniz",x, "olarak kaydedilimiştir.")
-
-
-
-    
-
