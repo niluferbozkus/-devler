004cddc3c2a99ebfd33ff8a3dde600cf3adc6877
niluferbozkus <72705898+niluferbozkus@users.noreply.github.com>
2020-11-11 09:02:08 +0300

    Update dörtislemhesap.py

--- dörtislemhesap.py
+++ dörtislemhesap.py
@@ -4,7 +4,7 @@ islemsayisi=int(input("Kaç işlem yapmak istiyorsunuz?: "))
 while count <= islemsayisi:
     sayi1 = int(input("İşlem yaapmak istediğiniz ilk sayıyı girin: "))
     sayi2 = int(input("İşlem yaapmak istediğiniz ikinci sayıyı girin: "))
-    op = input("Yapmak istediğiniz işlemi girin(+,-,*,/):" )
+    op = input("Yapmak istediğiniz işlem operatörünü girin(+,-,*,/):" )
     count = count+1
 
     if op in islem :
